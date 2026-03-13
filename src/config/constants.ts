3810810fa64503bdbb66a001932dc06108afe6af
Jimmy Essel <jimmykodwoessel@gmail.com>
2026-03-13 08:39:34 +0000

    add default templates

---
+++ src/config/constants.ts
@@ -0,0 +1,51 @@
+/**
+ * System Constants
+ */
+
+export const SYSTEM_ORG_ID = 'SYSTEM';
+
+export const DEFAULT_TEMPLATES = [
+    {
+        name: 'Male Full Body',
+        description: 'Standard measurement template for male full body garments',
+        fields: [
+            { name: 'Neck', unit: 'inches' },
+            { name: 'Chest', unit: 'inches' },
+            { name: 'Shoulder', unit: 'inches' },
+            { name: 'Sleeve Length', unit: 'inches' },
+            { name: 'Bicep', unit: 'inches' },
+            { name: 'Wrist', unit: 'inches' },
+            { name: 'Shirt Length', unit: 'inches' },
+            { name: 'Waist', unit: 'inches' },
+            { name: 'Hips', unit: 'inches' },
+            { name: 'Thigh', unit: 'inches' },
+            { name: 'Knee', unit: 'inches' },
+            { name: 'Ankle', unit: 'inches' },
+            { name: 'Trouser Length', unit: 'inches' },
+            { name: 'Inseam', unit: 'inches' },
+            { name: 'Crotch/Seat', unit: 'inches' }
+        ]
+    },
+    {
+        name: 'Female Full Body',
+        description: 'Standard measurement template for female full body garments',
+        fields: [
+            { name: 'Neck', unit: 'inches' },
+            { name: 'Bust', unit: 'inches' },
+            { name: 'Underbust', unit: 'inches' },
+            { name: 'Shoulder', unit: 'inches' },
+            { name: 'Sleeve Length', unit: 'inches' },
+            { name: 'Armhole', unit: 'inches' },
+            { name: 'Bicep', unit: 'inches' },
+            { name: 'Waist', unit: 'inches' },
+            { name: 'High Hip', unit: 'inches' },
+            { name: 'Full Hip', unit: 'inches' },
+            { name: 'Shoulder to Bust', unit: 'inches' },
+            { name: 'Shoulder to Waist', unit: 'inches' },
+            { name: 'Shoulder to Knee', unit: 'inches' },
+            { name: 'Full Length', unit: 'inches' },
+            { name: 'Thigh', unit: 'inches' },
+            { name: 'Ankle', unit: 'inches' }
+        ]
+    }
+];
